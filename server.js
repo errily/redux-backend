4c6050310b93ceb2ab9542f4e734347b013c301c
errily <erril.98@gmail.com>
2020-07-02 23:32:30 +0700

    update origin

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://mere-drop.surge.sh"
 };
 
 app.use(cors(corsOptions));
